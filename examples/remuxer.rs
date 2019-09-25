6542889a2e0b089bff693e6e8c8ec8687d322215
gibix <gibix@riseup.net>
2019-09-25 23:34:06 +0200

    add remux example

---
+++ examples/remuxer.rs
@@ -0,0 +1,75 @@
+#[macro_use]
+extern crate log;
+extern crate av_format;
+extern crate ivf;
+extern crate structopt;
+
+use av_format::buffer::AccReader;
+use av_format::demuxer::Context as DemuxerContext;
+use av_format::demuxer::Event;
+use av_format::muxer::Context as MuxerContext;
+use ivf::demuxer::*;
+use ivf::muxer::*;
+use std::fs::File;
+use std::sync::Arc;
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "ivf remux")]
+/// Simple Audio Video Encoding tool
+struct Opt {
+    /// Input file
+    #[structopt(short = "i", parse(from_os_str))]
+    input: PathBuf,
+    /// Output file
+    #[structopt(short = "o", parse(from_os_str))]
+    output: PathBuf,
+}
+
+fn main() {
+    let _ = pretty_env_logger::try_init();
+    let opt = Opt::from_args();
+
+    let input = std::fs::File::open(opt.input).unwrap();
+
+    let acc = AccReader::new(input);
+
+    let mut demuxer = DemuxerContext::new(Box::new(IvfDemuxer::new()), Box::new(acc));
+
+    demuxer.read_headers().unwrap();
+
+    trace!("global info: {:#?}", demuxer.info);
+
+    let output = File::create(opt.output).unwrap();
+
+    let mux = Box::new(IvfMuxer::new());
+
+    let mut muxer = MuxerContext::new(mux, Box::new(output));
+
+    muxer.set_global_info(demuxer.info.clone()).unwrap();
+    muxer.configure().unwrap();
+    muxer.write_header().unwrap();
+
+    loop {
+        match demuxer.read_event() {
+            Ok(event) => match event {
+                Event::MoreDataNeeded(sz) => panic!("we needed more data: {} bytes", sz),
+                Event::NewStream(s) => panic!("new stream :{:?}", s),
+                Event::NewPacket(packet) => {
+                    debug!("received packet with pos: {:?}", packet.pos);
+                    muxer.write_packet(Arc::new(packet)).unwrap();
+                }
+                Event::Eof => {
+                    muxer.write_trailer().unwrap();
+                    debug!("EOF!");
+                    break;
+                }
+            },
+            Err(e) => {
+                debug!("error: {:?}", e);
+                break;
+            }
+        }
+    }
+}
